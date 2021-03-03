b72a2730ba053f68abb28ded8eec2c696ddb03fd
Serakoi <admin@seer-software.com>
2021-03-03 22:05:24 +0100

    Fix some stuff lol

--- fun.js
+++ fun.js
@@ -1,10 +1,5 @@
 const inputArea = '.textArea-12jD-V'
 
 function ls_useLink(link){
-<<<<<<< HEAD
-    
+    // TODO : Add actual stuff here seer
 }
-=======
-
-}
->>>>>>> 9aabd146fd0643b4b98f127270e7a2080975c149
